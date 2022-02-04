82a176236d186e0e67677975ad109319a88c41d2
Stian Lyng <71536569+Stianlyng@users.noreply.github.com>
2022-02-04 01:45:05 +0100

    Create mouse_click_and_hold_titlebar.py

---
+++ mouse_click_and_hold_titlebar.py
@@ -0,0 +1,13 @@
+import pyautogui
+import time
+start = pyautogui.position()
+pyautogui.moveTo(Windows (X Position) + (Windows (Width)-15),Windows (Y Position)+7)
+old = pyautogui.position()
+#pyautogui.drag(30, 0, 10, button='left')
+
+while pyautogui.position() == old:
+	pyautogui.mouseDown(button='left')
+
+pyautogui.mouseUp(button='left')
+pyautogui.moveTo(start)
+quit()
